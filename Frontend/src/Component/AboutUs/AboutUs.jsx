68eb33ba0e501b0864353c6e179798e19a3d5218
kartavya21-dot <kartavyasharma0987@gmail.com>
2025-06-17 23:54:21 +0530

    Added more responsiveness, maybe last time for home page

--- Frontend/src/Component/AboutUs/AboutUs.jsx
+++ Frontend/src/Component/AboutUs/AboutUs.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import './AboutUs.css'
-import { photo } from '../../assets/assets'
+import React from "react";
+import "./AboutUs.css";
+import { photo } from "../../assets/assets";
 
 const AboutUs = () => {
   return (
-    <div className='about-us'>
-        <div className="text-area">
-            <p className='paragraph'>We believe in creating long-lasting products that blend durability with aesthetics, enhancing and beautifying the environment.</p>
+    <div className="about-us">
+      <div className="image">
+          <img src={photo.image} alt={photo.name} />
+      </div>
+      <div className="text-area">
+            <p className='paragraph'>" We believe in creating long-lasting products that blend durability with aesthetics, enhancing and beautifying the environment. "</p>
             <h1 className='heading'>-Dinesh Sharma</h1>
         </div>
-        <div className="image">
-            <img src={photo.image} alt={photo.name} />
-        </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
