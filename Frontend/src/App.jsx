1048275ae944561b8d56f62be817e4e972425e14
kartavya21-dot <kartavyasharma0987@gmail.com>
2025-06-19 01:17:36 +0530

    Starting of product gallery

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './assets/assets'
 import Navbar from './Component/Navbar/Navbar'
 import Home from './Pages/Home/Home'
 import Footer from './Component/Footer/Footer'
+import ProductDetail from './Pages/ProductDetail/ProductDetail'
+import { menu_items } from './assets/assets'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
       <Navbar/>
-      <Home/>
+      <div className="main-content">
+        <Home/>
+        {/* <ProductDetail product={menu_items[0]}/> */}
+      </div>
       <Footer/>
-    </>
+    </div>
   )
 }
 
--- Frontend/src/Pages/ProductDetail/ProductDetail.jsx
+++ Frontend/src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ProductDetail.css'
 
-const ProductDetail = ({}) => {
+const ProductDetail = ({product}) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const dd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <div>Gallery</div>
+    <div className='product-details-page'>
+      <div className="product-details">
+        <h1 className="product-details-name">{product.name}</h1>
+        <p className="product-details-price">{product.rate}</p>
+      </div>
+      <div className="main-image">
+        <img src={product.image} alt="" />
+      </div>
+      <div className="gallery">
+        {
+          dd.map((item, index) => {
+            return (
+              <div className="gallery-image-container" onClick={() => setImageIndex(index)}>
+                <img src="https://images.pexels.com/photos/30317240/pexels-photo-30317240.jpeg" alt="" />
+              </div>
+            )
+          } )
+        }
+      </div>
+    </div>
   )
 }
 
